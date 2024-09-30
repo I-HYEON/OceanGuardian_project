c132b252c74451cb275d3151f9489a0226040144
I-HYEON <tjwn1408@naver.com>
2024-09-30 14:40:30 +0900

    Update: cors Origins 추가

--- src/main/java/team/ivy/oceanguardian/global/config/WebConfig.java
+++ src/main/java/team/ivy/oceanguardian/global/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("http://localhost:3000", "http://localhost:8080", "https://badajikimi.site", "http://badajikimi.vercel.app")
+			.allowedOrigins("http://localhost:3000", "http://localhost:8080", "https://badajikimi.site", "http://badajikimi.vercel.app", "https://badajikimi.vercel.app")
 			.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS", "HEAD")
 			.allowedHeaders("Authorization", "Content-Type", "Accept", "X-Requested-With", "Cache-Control", "Origin", "Referer", "User-Agent", "Accept-Encoding", "Access-Control-Request-Headers")
 			.exposedHeaders("Authorization")
